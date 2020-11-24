bac32d59f857a1a8bf5238d4c03af8077b9eeefb
Grant <Grantemc2@att.net>
2020-11-24 16:31:38 -0600

    started server implamentation.

---
+++ server/server/Message.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace server
+{
+    class Message
+    {
+        public void StartServer()
+        {
+            // Start a websocket server at port 8001
+            var wss = new WebSocketServer(8001);
+
+            // Add the Echo websocket service
+            //wss.AddWebSocketService<Echo>("/echo");
+
+            // Add the Chat websocket service
+            //wss.AddWebSocketService<Chat>("/chat");
+
+            // Start the server
+            wss.Start();
+
+            Console.WriteLine("Press Enter to exit.");
+            Console.ReadLine();
+
+            // Stop the server
+            wss.Stop();
+        }
+
+        public void recieve(string message)
+        {
+
+        }
+
+        public void RecievedBid(BidItem x, int pos)
+        {
+
+        }
+
+        public void sendItems(List<BidItem> collection)
+        {
+
+        }
+    }
+}
---
+++ server/server/WebsocketCom.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace server
+{
+    class WebsocketCom
+    {
+        
+
+        public void updateclients()
+        {
+
+        }
+
+        public void notifycontroller()
+        {
+
+        }
+
+        public void MessageAllClients()
+        {
+
+        }
+
+        public void MessageSingleClient()
+        {
+
+        }
+
+        public void deserialize(string message)
+        {
+
+        }
+    }
+}
